09ebf8a97892215523fe4bb340d764fbaf200a9d
Branden Sherman <bshermandeveloper@gmail.com>
2020-05-14 15:52:00 -0400

    feat(canvas):created checkered background

---
+++ client/components/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Toolbar() {
+  return (
+    <div>
+      <div className='toolbar-container'>
+        <button className='tool-btn'>Pen</button>
+        <button className='tool-btn'>Brush</button>
+        <button className='tool-btn'>Erase</button>
+      </div>
+    </div>
+  );
+}
+
+export default Toolbar;
