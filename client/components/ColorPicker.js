673c0804c9e003e2f61082ec1540f6cdfd9d91c5
Branden Sherman <bshermandeveloper@gmail.com>
2020-05-18 11:30:25 -0400

    checking setState

--- client/components/ColorPicker.js
+++ client/components/ColorPicker.js
@@ -1,36 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { SketchPicker } from 'react-color';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Canvas from './Canvas';
-import AnimateSprite from './AnimateSprite';
 
-function MainPage() {
+const ColorPicker = (props) => {
   const initialState = '#000000';
   const [currentColor, setColor] = useState(initialState);
   const [editMode, setMode] = useState(true);
 
   function handleChangeComplete(color) {
-    // console.log('color >>> ', color) // returns obj
     setColor(color.hex);
-    // can't set alpha with hex - ignore for now
   }
 
   useEffect(() => {
-    // document.title = currentColor;
-  });
+    props.currentColor(currentColor);
+  }, [currentColor]);
+
+  console.log(props);
 
   return (
-    <div className='container main-container'>
-      <div className='left-container'>
+    <div className='main-container'>
+      <div>
         <SketchPicker
-          disableAlpha={true}
           className='sketch'
           color={currentColor}
+          disableAlpha={true}
           onChangeComplete={handleChangeComplete}
         />
-
-
+      </div>
+    </div>
   );
-}
+};
 
-export default MainPage;
+export default ColorPicker;
---
+++ client/components/DeleteFrame.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { SketchPicker } from 'react-color';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const DeleteFrame = (props) => {
+  // const filteredArray = this.state.framesArray.filter(
+  //   (frame) => frame !== canvasName
+  // )
+
+  console.log(props);
+
+  return <button>DELETE</button>;
+};
+
+export default DeleteFrame;
