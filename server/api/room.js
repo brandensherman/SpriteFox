a9250d27810af447abe3ba62dd2d01ac945aca07
HagenGaryP <hagengaryp@gmail.com>
2020-06-01 15:53:52 -0400

    cleaned the files

--- server/api/room.js
+++ server/api/room.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const {Room, Frame} = require('../db/models');
 
+// not implemented at the moment, but keeping the code for when we use a DB/APIs
 router.get('/:hash', async (req, res, next) => {
   try {
     const room = await Room.findOne({
